73ffdc7bc72385df62ee51e1e68090e665724550
Nisa Eka <nisa.eka.juliana@gmail.com>
2024-01-19 10:09:29 +0700

    feature: save token to local storage add logout button

--- lib/screen/profile/profile_controller.dart
+++ lib/screen/profile/profile_controller.dart
@@ -1,5 +1,15 @@
 import 'package:css_mobile/base/base_controller.dart';
+import 'package:css_mobile/screen/auth/login/login_screen.dart';
+import 'package:get/get.dart';
 
-class ProfileController extends BaseController{
+class ProfileController extends BaseController {
+  @override
+  void onInit() {
+    super.onInit();
+  }
 
+  void doLogout() async {
+    storage.deleteToken();
+    Get.offAll(const LoginScreen());
+  }
 }
--- lib/screen/profile/profile_screen.dart
+++ lib/screen/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:css_mobile/const/color_const.dart';
 import 'package:css_mobile/const/textstyle.dart';
+import 'package:css_mobile/screen/profile/profile_controller.dart';
 import 'package:css_mobile/widgets/bar/custombackbutton.dart';
 import 'package:css_mobile/widgets/bar/custombottombar.dart';
 import 'package:css_mobile/widgets/profile/user_info_card.dart';
@@ -11,43 +12,50 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: whiteColor,
-        title: Text("Profil".tr, style: appTitleTextStyle.copyWith(color: blueJNE)),
-        leading: const CustomBackButton(),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: greyLightColor3, width: 5)
-              )
+    return GetBuilder<ProfileController>(
+        init: ProfileController(),
+        builder: (controller) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: whiteColor,
+              title: Text("Profil".tr, style: appTitleTextStyle.copyWith(color: blueJNE)),
+              leading: const CustomBackButton(),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            body: ListView(
               children: [
-                const UserInfoCard(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.edit_square,
-                    color: redJNE,
-                    size: 25,
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: greyLightColor3, width: 5))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const UserInfoCard(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit_square,
+                          color: redJNE,
+                          size: 25,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                ListTile(
+                  leading: const Icon(Icons.account_box, color: blueJNE, size: 30),
+                  title: Text('Keluar'.tr, style: listTitleTextStyle),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.logout, color: redJNE, size: 30),
+                    onPressed: () => controller.doLogout(),
+                  ),
+                  onTap: () => controller.doLogout(),
+                )
               ],
             ),
-          ),
-          ListTile(
-          )
-        ],
-      ),
-      bottomNavigationBar: const BottomBar(
-        menu: 1,
-      ),
-    );
+            bottomNavigationBar: const BottomBar(
+              menu: 1,
+            ),
+          );
+        });
   }
 }
