52700b41a1c7bd98c3462b678bc1c872c514a608
nisae <nisa.eka.juliana@gmail.com>
2024-06-20 14:06:41 +0700

    feature: - setup push notification

--- lib/config/firebase_config.dart
+++ lib/config/firebase_config.dart
@@ -2,8 +2,8 @@ import 'package:css_mobile/api/firebase_api.dart';
 import 'package:css_mobile/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-class CssFirebaseConfig{
-  static Future<void> init()async{
+class CssFirebaseConfig {
+  static Future<void> init() async {
     await Firebase.initializeApp(
       options: CssFirebaseOptions.currentPlatform,
     );
