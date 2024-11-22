51f0a0a1adf2488a46e4cf205a43f7bbf6591e18
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-11-22 13:56:15 +0700

    feat: slicing detail kiriman screen

--- lib/data/model/request_pickup/request_pickup_detail_model.dart
+++ lib/data/model/request_pickup/request_pickup_detail_model.dart
@@ -1,32 +1,62 @@
 class RequestPickupDetailModel {
   final String awb;
-  final String petugasEntry;
   final String createdDateSearch;
   final String apiType;
   final String serviceCode;
-  final num? codAmount;
   final String shipperName;
   final String shipperCity;
   final String receiverName;
   final String receiverCity;
   final String receiverPhone;
+  final num? codAmount;
+  final String? pickupName;
+  final String? pickupPicPhone;
+  final String? pickupCity;
+  final String? pickupDistrict;
+  final String? pickupAddress;
+  final String? pickupService;
+  final String? pickupVehicle;
+  final String? pickupDate;
+  final String? pickupTime;
+  final String? pickupPic;
+  final String? statusDesc;
+  final String? custId;
+  final String petugasEntry;
+  final String? goodDesc;
+  final num? weight;
   final String status;
   final String? pickupStatus;
+  final String? infoStatus;
 
   RequestPickupDetailModel({
     required this.awb,
-    required this.petugasEntry,
     required this.createdDateSearch,
     required this.apiType,
     required this.serviceCode,
-    required this.codAmount,
     required this.shipperName,
     required this.shipperCity,
     required this.receiverName,
     required this.receiverCity,
     required this.receiverPhone,
+    this.codAmount,
+    this.pickupName,
+    this.pickupPicPhone,
+    this.pickupCity,
+    this.pickupDistrict,
+    this.pickupAddress,
+    this.pickupService,
+    this.pickupVehicle,
+    this.pickupDate,
+    this.pickupTime,
+    this.pickupPic,
+    this.statusDesc,
+    this.custId,
+    required this.petugasEntry,
+    this.goodDesc,
+    this.weight,
     required this.status,
-    required this.pickupStatus,
+    this.pickupStatus,
+    this.infoStatus,
   });
 
   factory RequestPickupDetailModel.fromJson(Map<String, dynamic> json) =>
@@ -36,7 +66,7 @@ class RequestPickupDetailModel {
         createdDateSearch: json['createdDateSearch'] ?? '',
         apiType: json['apiType'] ?? '',
         serviceCode: json['serviceCode'] ?? '',
-        codAmount: json['codAmount'] ?? 0,
+        codAmount: json['codAmount'],
         shipperName: json['shipperName'] ?? '',
         shipperCity: json['shipperCity'] ?? '',
         receiverName: json['receiverName'] ?? '',
@@ -44,5 +74,21 @@ class RequestPickupDetailModel {
         receiverPhone: json['receiverPhone'] ?? '',
         status: json['status'] ?? '',
         pickupStatus: json['pickupStatus'] ?? '',
+        // Optional fields with nullable handling
+        pickupName: json['pickupName'],
+        pickupPicPhone: json['pickupPicPhone'],
+        pickupCity: json['pickupCity'],
+        pickupDistrict: json['pickupDistrict'],
+        pickupAddress: json['pickupAddress'],
+        pickupService: json['pickupService'],
+        pickupVehicle: json['pickupVehicle'],
+        pickupDate: json['pickupDate'],
+        pickupTime: json['pickupTime'],
+        pickupPic: json['pickupPic'],
+        statusDesc: json['statusDesc'],
+        custId: json['custId'],
+        goodDesc: json['goodDesc'],
+        weight: json['weight'],
+        infoStatus: json['infoStatus'],
       );
 }
