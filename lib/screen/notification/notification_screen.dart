a26dcf6fe67e65a04140d2f333b43734808375eb
Ravenaa <ravena0402@gmail.com>
2024-12-04 09:22:57 +0700

    init commit add darkmode and shimmer on screen pantau paketmu detail

--- lib/screen/notification/notification_screen.dart
+++ lib/screen/notification/notification_screen.dart
@@ -1,8 +1,5 @@
 import 'package:css_mobile/screen/notification/notification_controller.dart';
 import 'package:css_mobile/widgets/bar/customtopbar.dart';
-import 'package:css_mobile/widgets/dialog/data_empty_dialog.dart';
-import 'package:css_mobile/widgets/dialog/loading_dialog.dart';
-import 'package:css_mobile/widgets/items/notification_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -20,45 +17,45 @@ class NotificationScreen extends StatelessWidget {
               appBar: CustomTopBar(
                 title: "Notifikasi".tr,
               ),
-              body: controller.isLoading
-                  ? const LoadingDialog()
-                  : _bodyContent(controller, context),
+              // body: controller.isLoading
+              //     ? const LoadingDialog()
+              //     : _bodyContent(controller, context),
             ),
           );
         });
   }
 
-  Widget _bodyContent(NotificationController c, BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverList.list(
-          children: c.unreadNotifList
-              .map(
-                (e) => e.id == null
-                    ? const SizedBox()
-                    : NotificationListItem(
-                        data: e,
-                        isRead: true,
-                        onTap: () => c.readMessage(e.id ?? ''),
-                      ),
-              )
-              .toList(),
-        ),
-        c.notificationList.isNotEmpty
-            ? SliverList.list(
-                children: c.notificationList
-                    .map(
-                      (e) => NotificationListItem(
-                        data: e,
-                        // onTap: () => c.readMessage(e.id ?? ''),
-                      ),
-                    )
-                    .toList(),
-              )
-            : DataEmpty(
-                text: "Belum ada notifikasi tersedia".tr,
-              ),
-      ],
-    );
-  }
+  // Widget _bodyContent(NotificationController c, BuildContext context) {
+  //   return CustomScrollView(
+  //     slivers: [
+  //       SliverList.list(
+  //         children: c.unreadNotifList
+  //             .map(
+  //               (e) => e.id == null
+  //                   ? const SizedBox()
+  //                   : NotificationListItem(
+  //                       data: e,
+  //                       isRead: true,
+  //                       onTap: () => c.readMessage(e.id ?? ''),
+  //                     ),
+  //             )
+  //             .toList(),
+  //       ),
+  //       c.notificationList.isNotEmpty
+  //           ? SliverList.list(
+  //               children: c.notificationList
+  //                   .map(
+  //                     (e) => NotificationListItem(
+  //                       data: e,
+  //                       // onTap: () => c.readMessage(e.id ?? ''),
+  //                     ),
+  //                   )
+  //                   .toList(),
+  //             )
+  //           : DataEmpty(
+  //               text: "Belum ada notifikasi tersedia".tr,
+  //             ),
+  //     ],
+  //   );
+  // }
 }
