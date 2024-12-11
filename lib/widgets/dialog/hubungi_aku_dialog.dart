5e4d7c77e89f7d991730150858442ecb8488c0b3
nisae <nisa.eka.juliana@gmail.com>
2024-12-11 15:25:43 +0700

    fixing: - fix icon on hubungi aku dialog

--- lib/widgets/dialog/hubungi_aku_dialog.dart
+++ lib/widgets/dialog/hubungi_aku_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:css_mobile/const/color_const.dart';
-import 'package:css_mobile/const/icon_const.dart';
+import 'package:css_mobile/const/image_const.dart';
 import 'package:css_mobile/data/model/auth/post_login_model.dart';
 import 'package:css_mobile/screen/hubungi_aku/eclaim/add/add_eclaim_screen.dart';
 import 'package:css_mobile/screen/hubungi_aku/laporanku/input/input_laporanku_screen.dart';
@@ -51,9 +51,10 @@ class HubungiAkuDialog extends StatelessWidget {
           allow.hubungiLaporan == "Y" || allow.laporan == "Y"
               ? ListTile(
                   leading: const MenuIcon(
-                    icon: IconsConstant.ticket,
+                    // icon: IconsConstant.ticket,
+                    icon: ImageConstant.hubungiAkuIcon,
                     padding: EdgeInsets.all(1),
-                    isTransparent: true,
+                    // isTransparent: true,
                     isActive: false,
                   ),
                   title: Text(
@@ -70,9 +71,10 @@ class HubungiAkuDialog extends StatelessWidget {
           allow.hubungiEclaim == "Y" || allow.eclaim == "Y"
               ? ListTile(
                   leading: const MenuIcon(
-                    icon: IconsConstant.eclaim,
+                    // icon: IconsConstant.eclaim,
+                    icon: ImageConstant.hubungiAkuIcon,
                     padding: EdgeInsets.all(1),
-                    isTransparent: true,
+                    // isTransparent: true,
                     isActive: false,
                   ),
                   title: Text(
