b4de0223d60155ca9912c910879c4afb117ab820
Ravenaa <ravena0402@gmail.com>
2024-12-12 18:01:27 +0700

    fix filter on screen invoice

---
+++ lib/screen/invoice/components/invoice_filter_button.dart
@@ -0,0 +1,151 @@
+import 'package:css_mobile/screen/invoice/invoice_controller.dart';
+import 'package:css_mobile/util/ext/string_ext.dart';
+import 'package:css_mobile/widgets/bar/filter_button.dart';
+import 'package:css_mobile/widgets/forms/customformlabel.dart';
+import 'package:css_mobile/widgets/forms/customradiobutton.dart';
+import 'package:css_mobile/widgets/forms/customtextformfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
+
+class InvoiceFilterButton extends HookWidget {
+  const InvoiceFilterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<InvoiceController>(
+        init: InvoiceController(),
+        builder: (c) {
+          return FilterButton(
+            filterContent: StatefulBuilder(
+              builder: (context, setState) {
+                return Expanded(
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomFormLabel(label: 'Tanggal Entry'.tr),
+                            const SizedBox(height: 10),
+                            Customradiobutton(
+                              title: "Semua Tanggal".tr,
+                              value: '0',
+                              groupValue: c.state.dateFilter,
+                              onChanged: (value) =>
+                                  setState(() => c.selectDateFilter(0)),
+                              onTap: () =>
+                                  setState(() => c.selectDateFilter(0)),
+                            ),
+                            Customradiobutton(
+                              title: "1 Bulan Terakhir".tr,
+                              value: '1',
+                              groupValue: c.state.dateFilter,
+                              onChanged: (value) =>
+                                  setState(() => c.selectDateFilter(1)),
+                              onTap: () =>
+                                  setState(() => c.selectDateFilter(1)),
+                            ),
+                            Customradiobutton(
+                              title: "1 Minggu Terakhir".tr,
+                              value: '2',
+                              groupValue: c.state.dateFilter,
+                              onChanged: (value) =>
+                                  setState(() => c.selectDateFilter(2)),
+                              onTap: () =>
+                                  setState(() => c.selectDateFilter(2)),
+                            ),
+                            Customradiobutton(
+                              title: "Hari Ini".tr,
+                              value: '3',
+                              groupValue: c.state.dateFilter,
+                              onChanged: (value) =>
+                                  setState(() => c.selectDateFilter(3)),
+                              onTap: () =>
+                                  setState(() => c.selectDateFilter(3)),
+                            ),
+                            Customradiobutton(
+                              title: "Pilih Tanggal Sendiri".tr,
+                              value: '4',
+                              groupValue: c.state.dateFilter,
+                              onChanged: (value) =>
+                                  setState(() => c.selectDateFilter(4)),
+                              onTap: () =>
+                                  setState(() => c.selectDateFilter(4)),
+                            ),
+                            //Pilih Tanggal Sendiri
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                CustomTextFormField(
+                                  controller: c.state.startDateField,
+                                  readOnly: true,
+                                  width: Get.width / 2.3,
+                                  hintText: 'Tanggal Awal'.tr,
+                                  onTap: () =>
+                                      c.selectDate(context).then((value) {
+                                    setState(() {
+                                      c.selectedDateStart = value;
+                                      c.state.startDateField.text =
+                                          value.toString().toDateTimeFormat();
+                                      c.selectedDateEnd = DateTime.now();
+                                      c.state.endDateField.text = DateTime.now()
+                                          .toString()
+                                          .toDateTimeFormat();
+                                      c.update();
+                                    });
+                                  }),
+                                  // hintText: 'Dari Tanggal',
+                                ),
+                                CustomTextFormField(
+                                  controller: c.state.endDateField,
+                                  readOnly: true,
+                                  width: Get.width / 2.3,
+                                  hintText: 'Tanggal Akhir'.tr,
+                                  onTap: () =>
+                                      c.selectDate(context).then((value) {
+                                    setState(() {
+                                      c.selectedDateEnd = value;
+                                      c.state.endDateField.text =
+                                          value.toString().toDateTimeFormat();
+                                      c.update();
+                                    });
+                                  }),
+                                ),
+                              ],
+                            ),
+                            // CustomFormLabel(label: 'Status Kiriman'.tr),
+                            // const SizedBox(height: 10),
+                            // CustomFormLabel(label: 'Petugas Entry'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            isFiltered: c.state.isFiltered,
+            // isApplyFilter: c.state.startDate != null || c.state.endDate != null,
+            onResetFilter: () {
+              // c.resetFilter();
+              c.selectDateFilter(3);
+              c.applyFilter();
+              Get.back();
+            },
+            onApplyFilter: () {
+              // c.applyFilter();
+              c.applyFilter();
+              Get.back();
+            },
+            onCloseFilter: () {
+              if (!c.state.isFiltered) {
+                c.resetFilter();
+              } else {
+                Get.back();
+              }
+            },
+          );
+        });
+  }
+}
--- lib/screen/invoice/invoice_controller.dart
+++ lib/screen/invoice/invoice_controller.dart
@@ -1,15 +1,20 @@
 import 'package:css_mobile/base/base_controller.dart';
+import 'package:css_mobile/base/theme_controller.dart';
 import 'package:css_mobile/data/model/invoice/invoice_model.dart';
 import 'package:css_mobile/data/model/query_model.dart';
 import 'package:css_mobile/data/model/request_pickup/request_pickup_date_enum.dart';
+import 'package:css_mobile/screen/invoice/invoice_state.dart';
 import 'package:css_mobile/util/constant.dart';
 import 'package:css_mobile/util/ext/date_ext.dart';
+import 'package:css_mobile/util/ext/string_ext.dart';
 import 'package:css_mobile/util/logger.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class InvoiceController extends BaseController {
+  final state = InvoiceState();
+
   // final AdvanceFilterModel _advanceFilterModel = AdvanceFilterModel();
   final QueryModel _queryParamModel = QueryModel();
 
@@ -39,106 +44,111 @@ class InvoiceController extends BaseController {
     pagingController.addPageRequestListener((pageKey) {
       _getInvoices(pageKey);
     });
-  }
 
-  void applyFilterDate() {
-    bool startDateAvailable = selectedDateStart != null;
-    bool endDateAvailable = selectedDateEnd != null;
-
-    List<Map<String, dynamic>> between = [];
-
-    switch (selectedFilterDate) {
-      case RequestPickupDateEnum.custom:
-        if (startDateAvailable && endDateAvailable) {
-          between.add({
-            "invoiceDate": [
-              DateTime(selectedDateStart!.year, selectedDateStart!.month,
-                      selectedDateStart!.day)
-                  .toIso8601String(),
-              DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
-                      selectedDateEnd!.day, 23, 59, 59, 999)
-                  .toIso8601String()
-            ]
-          });
-          filterDateText = '$selectedDateStartText - $selectedDateEndText';
-        } else if (startDateAvailable) {
-          between.add({
-            "invoiceDate": [
-              DateTime(selectedDateStart!.year, selectedDateStart!.month,
-                      selectedDateStart!.day)
-                  .toIso8601String(),
-              DateTime(selectedDateStart!.year, selectedDateStart!.month,
-                      selectedDateStart!.day, 23, 59, 59, 999)
-                  .toIso8601String()
-            ]
-          });
-          filterDateText = selectedDateStartText;
-        } else if (endDateAvailable) {
-          between.add({
-            "invoiceDate": [
-              DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
-                      selectedDateEnd!.day)
-                  .toIso8601String(),
-              DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
-                      selectedDateEnd!.day, 23, 59, 59, 999)
-                  .toIso8601String()
-            ]
-          });
-          filterDateText = selectedDateEndText;
-        }
-        break;
-      case RequestPickupDateEnum.all:
-        between = [];
-        break;
-      case RequestPickupDateEnum.oneMonth:
-        final currentDateTime = DateTime.now();
-        final oneMonthAgo = currentDateTime.subtract(const Duration(days: 30));
-        between.add({
-          "invoiceDate": [
-            DateTime(oneMonthAgo.year, oneMonthAgo.month, oneMonthAgo.day)
-                .toIso8601String(),
-            DateTime(currentDateTime.year, currentDateTime.month,
-                    currentDateTime.day, 23, 59, 59, 999)
-                .toIso8601String()
-          ]
-        });
-        break;
-      case RequestPickupDateEnum.oneWeek:
-        final currentDateTime = DateTime.now();
-        final oneWeekAgo = currentDateTime.subtract(const Duration(days: 7));
-        between.add({
-          "invoiceDate": [
-            DateTime(oneWeekAgo.year, oneWeekAgo.month, oneWeekAgo.day)
-                .toIso8601String(),
-            DateTime(currentDateTime.year, currentDateTime.month,
-                    currentDateTime.day, 23, 59, 59, 999)
-                .toIso8601String()
-          ]
-        });
-        break;
-      case RequestPickupDateEnum.today:
-        final currentDateTime = DateTime.now();
-        final startOfDay = DateTime(
-            currentDateTime.year, currentDateTime.month, currentDateTime.day);
-        final endOfDay = DateTime(currentDateTime.year, currentDateTime.month,
-            currentDateTime.day, 23, 59, 59, 999);
-        between.add({
-          "invoiceDate": [
-            startOfDay.toIso8601String(),
-            endOfDay.toIso8601String()
-          ]
-        });
-        break;
-      default:
-        between = [];
-        break;
-    }
-
-    _queryParamModel.setBetween(between);
-
+    selectDateFilter(3);
+    applyFilter();
     requireRetry();
-    update();
+    // applyFilter();
   }
+
+  // void applyFilterDate() {
+  //   bool startDateAvailable = selectedDateStart != null;
+  //   bool endDateAvailable = selectedDateEnd != null;
+  //
+  //   List<Map<String, dynamic>> between = [];
+  //
+  //   switch (selectedFilterDate) {
+  //     case RequestPickupDateEnum.custom:
+  //       if (startDateAvailable && endDateAvailable) {
+  //         between.add({
+  //           "invoiceDate": [
+  //             DateTime(selectedDateStart!.year, selectedDateStart!.month,
+  //                     selectedDateStart!.day)
+  //                 .toIso8601String(),
+  //             DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
+  //                     selectedDateEnd!.day, 23, 59, 59, 999)
+  //                 .toIso8601String()
+  //           ]
+  //         });
+  //         filterDateText = '$selectedDateStartText - $selectedDateEndText';
+  //       } else if (startDateAvailable) {
+  //         between.add({
+  //           "invoiceDate": [
+  //             DateTime(selectedDateStart!.year, selectedDateStart!.month,
+  //                     selectedDateStart!.day)
+  //                 .toIso8601String(),
+  //             DateTime(selectedDateStart!.year, selectedDateStart!.month,
+  //                     selectedDateStart!.day, 23, 59, 59, 999)
+  //                 .toIso8601String()
+  //           ]
+  //         });
+  //         filterDateText = selectedDateStartText;
+  //       } else if (endDateAvailable) {
+  //         between.add({
+  //           "invoiceDate": [
+  //             DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
+  //                     selectedDateEnd!.day)
+  //                 .toIso8601String(),
+  //             DateTime(selectedDateEnd!.year, selectedDateEnd!.month,
+  //                     selectedDateEnd!.day, 23, 59, 59, 999)
+  //                 .toIso8601String()
+  //           ]
+  //         });
+  //         filterDateText = selectedDateEndText;
+  //       }
+  //       break;
+  //     case RequestPickupDateEnum.all:
+  //       between = [];
+  //       break;
+  //     case RequestPickupDateEnum.oneMonth:
+  //       final currentDateTime = DateTime.now();
+  //       final oneMonthAgo = currentDateTime.subtract(const Duration(days: 30));
+  //       between.add({
+  //         "invoiceDate": [
+  //           DateTime(oneMonthAgo.year, oneMonthAgo.month, oneMonthAgo.day)
+  //               .toIso8601String(),
+  //           DateTime(currentDateTime.year, currentDateTime.month,
+  //                   currentDateTime.day, 23, 59, 59, 999)
+  //               .toIso8601String()
+  //         ]
+  //       });
+  //       break;
+  //     case RequestPickupDateEnum.oneWeek:
+  //       final currentDateTime = DateTime.now();
+  //       final oneWeekAgo = currentDateTime.subtract(const Duration(days: 7));
+  //       between.add({
+  //         "invoiceDate": [
+  //           DateTime(oneWeekAgo.year, oneWeekAgo.month, oneWeekAgo.day)
+  //               .toIso8601String(),
+  //           DateTime(currentDateTime.year, currentDateTime.month,
+  //                   currentDateTime.day, 23, 59, 59, 999)
+  //               .toIso8601String()
+  //         ]
+  //       });
+  //       break;
+  //     case RequestPickupDateEnum.today:
+  //       final currentDateTime = DateTime.now();
+  //       final startOfDay = DateTime(
+  //           currentDateTime.year, currentDateTime.month, currentDateTime.day);
+  //       final endOfDay = DateTime(currentDateTime.year, currentDateTime.month,
+  //           currentDateTime.day, 23, 59, 59, 999);
+  //       between.add({
+  //         "invoiceDate": [
+  //           startOfDay.toIso8601String(),
+  //           endOfDay.toIso8601String()
+  //         ]
+  //       });
+  //       break;
+  //     default:
+  //       between = [];
+  //       break;
+  //   }
+  //
+  //   _queryParamModel.setBetween(between);
+  //
+  //   requireRetry();
+  //   update();
+  // }
 
   Future<void> _getInvoiceCount() async {
     try {
@@ -211,5 +221,129 @@ class InvoiceController extends BaseController {
       selectedDateEnd = dateTime;
       selectedDateEndText = dateTime.toStringWithFormat(format: "dd MMM yyyy");
     }
+  }
+
+  void selectDateFilter(int filter) {
+    state.dateFilter = filter.toString();
+    update();
+    if (filter == 0 || filter == 4) {
+      selectedDateStart = null;
+      selectedDateEnd = null;
+      state.startDateField.clear();
+      state.endDateField.clear();
+      // state.transDate = [];
+    } else if (filter == 1) {
+      selectedDateStart = DateTime.now()
+          .copyWith(hour: 0, minute: 0)
+          .subtract(const Duration(days: 30));
+      selectedDateEnd =
+          DateTime.now().copyWith(hour: 23, minute: 59, second: 59);
+      state.startDateField.text =
+          selectedDateStart.toString().toLongDateTimeFormat();
+      state.endDateField.text =
+          selectedDateEnd.toString().toLongDateTimeFormat();
+    } else if (filter == 2) {
+      selectedDateStart = DateTime.now()
+          .copyWith(hour: 0, minute: 0)
+          .subtract(const Duration(days: 7));
+      selectedDateEnd =
+          DateTime.now().copyWith(hour: 23, minute: 59, second: 59);
+      state.startDateField.text =
+          selectedDateStart.toString().toLongDateTimeFormat();
+      state.endDateField.text =
+          selectedDateEnd.toString().toLongDateTimeFormat();
+    } else if (filter == 3) {
+      selectedDateStart = DateTime.now().copyWith(hour: 0, minute: 0);
+      selectedDateEnd =
+          DateTime.now().copyWith(hour: 23, minute: 59, second: 59);
+      state.startDateField.text =
+          selectedDateStart.toString().toLongDateTimeFormat();
+      state.endDateField.text =
+          selectedDateEnd.toString().toLongDateTimeFormat();
+    }
+    update();
+  }
+
+  Future<DateTime?> selectDate(BuildContext context) async {
+    // Show date picker
+    final DateTime? selectedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+      builder: (context, child) {
+        return Theme(
+          data: CustomTheme().dateTimePickerTheme(context),
+          child: child!,
+        );
+      },
+    );
+
+    if (selectedDate == null || !context.mounted) return null;
+
+    // Show time picker
+    final TimeOfDay? selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      builder: (context, child) {
+        return Theme(
+          data: CustomTheme().dateTimePickerTheme(context),
+          child: child!,
+        );
+      },
+    );
+
+    if (selectedTime == null || !context.mounted) return null;
+
+    // Combine date and time
+    return DateTime(
+      selectedDate.year,
+      selectedDate.month,
+      selectedDate.day,
+      selectedTime.hour,
+      selectedTime.minute,
+    );
+  }
+
+  void resetFilter() {
+    selectedDateStart = null;
+    selectedDateEnd = null;
+    state.startDateField.clear();
+    state.endDateField.clear();
+    // state.isFiltered = false;
+    state.searchField.clear();
+    state.transDate = [];
+    state.dateFilter = '0';
+    state.pagingController.refresh();
+    update();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    state.pagingController.dispose();
+  }
+
+  applyFilter() {
+    // if (selectedDateStart != null ||
+    //     selectedDateEnd != null ||
+    //     state.selectedPetugasEntry != null ||
+    //     state.selectedStatusKiriman != null) {
+    state.isFiltered = true;
+    if (selectedDateStart != null && selectedDateEnd != null) {
+      state.transDate = [
+        {
+          "invoiceDate": ["$selectedDateStart", "$selectedDateEnd"]
+        }
+      ];
+      // "${selectedDateStart?.millisecondsSinceEpoch ?? ''}-${selectedDateEnd?.millisecondsSinceEpoch ?? ''}";
+    }
+    if (state.dateFilter == '0') {
+      resetFilter();
+    }
+
+    _queryParamModel.setBetween(state.transDate);
+    requireRetry();
+    update();
   }
 }
