e111aef7e558a6960efb7f8c9e2d7d8c1606bf6b
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-12-13 16:21:13 +0700

    refactor: add shimmer on jlc widget dashboard

--- lib/screen/dashboard/components/jlcpoint_widget.dart
+++ lib/screen/dashboard/components/jlcpoint_widget.dart
@@ -3,6 +3,7 @@ import 'package:css_mobile/const/image_const.dart';
 import 'package:css_mobile/screen/bonus_kamu/bonus_kamu_screen.dart';
 import 'package:css_mobile/screen/dashboard/dashboard_controller.dart';
 import 'package:css_mobile/util/ext/string_ext.dart';
+import 'package:css_mobile/widgets/dialog/shimer_loading_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,29 +22,45 @@ class JLCPointWidget extends StatelessWidget {
                       controller.state.allow.bonus == "Y")
               ? GestureDetector(
                   onTap: () => Get.to(const BonusKamuScreen()),
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).brightness == Brightness.light
-                          ? whiteColor
-                          : greyColor,
-                      borderRadius: BorderRadius.circular(50),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: redJNE,
-                          spreadRadius: 1,
-                          offset: Offset(-2, 3),
+                  child: Shimmer(
+                    isLoading: controller.state.isLoading,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: controller.state.isLoading
+                            ? greyColor
+                            : Colors.transparent,
+                        borderRadius: controller.state.isLoading
+                            ? BorderRadius.circular(50)
+                            : null,
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color:
+                              Theme.of(context).brightness == Brightness.light
+                                  ? whiteColor
+                                  : greyColor,
+                          borderRadius: BorderRadius.circular(50),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: redJNE,
+                              spreadRadius: 1,
+                              offset: Offset(-2, 3),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(ImageConstant.logoJLC, height: 14),
-                        Text(' ${point != '0' ? point.toDouble() : 0} Point'),
-                      ],
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset(ImageConstant.logoJLC, height: 14),
+                            Text(
+                                ' ${point != '0' ? point.toDouble() : 0} Point'),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 )
