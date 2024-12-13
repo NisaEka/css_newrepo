7dad3f2b7e84cb73c90e26604bf910491fc5eb12
Mochamad Nurul Huda <mochamadnurulhuda16@gmail.com>
2024-12-13 17:39:47 +0700

    refactor: add shimmer to promo and news on dashboard

--- lib/widgets/items/news_item.dart
+++ lib/widgets/items/news_item.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:css_mobile/const/color_const.dart';
 import 'package:css_mobile/data/model/dashboard/dashboard_banner_model.dart';
 import 'package:css_mobile/data/model/dashboard/dashboard_news_model.dart';
+import 'package:css_mobile/util/snackbar.dart';
 import 'package:css_mobile/widgets/dialog/shimer_loading_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -26,65 +27,100 @@ class NewsItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => _launchUrl(),
-      child: Shimmer(
-        isLoading: isLoading,
-        child: Container(
-          margin: const EdgeInsets.all(10),
-          width: Get.width / 2,
-          color: isLoading ? greyColor : Colors.transparent,
-          // height: promo != null ? Get.width / 2 : null,
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () => _showImagePreview(
-                  context,
-                  news?.thumbnail ?? promo?.picture ?? '',
-                ),
-                child: CachedNetworkImage(
-                  fit: BoxFit.contain,
-                  imageUrl: news?.thumbnail ?? promo?.picture ?? '',
-                  width: Get.width / 2,
-                  height: promo != null ? Get.width / 2 : null,
-                  placeholder: (context, url) => Center(
-                    child: CircularProgressIndicator(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  errorWidget: (context, url, error) => const Center(
-                    child: Icon(
-                      Icons.broken_image,
-                      color: redJNE,
-                      size: 50,
-                    ),
-                  ),
-                ),
-              ),
-              Text(
-                lang == "id"
-                    ? news?.detail?.where((e) => e.lang == "id").first.title ??
-                        promo?.namaBanner ??
-                        ''
-                    : news?.detail?.where((e) => e.lang == "en").first.title ??
-                        promo?.namaBanner ??
-                        '',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleMedium,
-              )
-            ],
-          ),
-        ),
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        width: Get.width / 2,
+        child: isLoading
+            ? _buildShimmerPlaceholder(context)
+            : _buildContent(context),
       ),
     );
   }
 
+  Widget _buildShimmerPlaceholder(BuildContext context) {
+    return Shimmer(
+      isLoading: true,
+      child: Column(
+        children: [
+          Container(
+            width: Get.width / 2,
+            height: Get.width / 2,
+            decoration: BoxDecoration(
+              color: greyColor,
+              borderRadius: BorderRadius.circular(6),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            width: Get.width / 3,
+            height: 16,
+            decoration: BoxDecoration(
+              color: greyColor,
+              borderRadius: BorderRadius.circular(3),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () => _showImagePreview(
+            context,
+            news?.thumbnail ?? promo?.picture ?? '',
+          ),
+          child: CachedNetworkImage(
+            fit: BoxFit.contain,
+            imageUrl: news?.thumbnail ?? promo?.picture ?? '',
+            width: Get.width / 2,
+            height: promo != null ? Get.width / 2 : null,
+            placeholder: (context, url) => Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            errorWidget: (context, url, error) => const Center(
+              child: Icon(
+                Icons.broken_image,
+                color: redJNE,
+                size: 50,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          lang == "id"
+              ? news?.detail?.where((e) => e.lang == "id").first.title ??
+                  promo?.namaBanner ??
+                  ''
+              : news?.detail?.where((e) => e.lang == "en").first.title ??
+                  promo?.namaBanner ??
+                  '',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.titleMedium,
+        )
+      ],
+    );
+  }
+
   Future<void> _launchUrl() async {
-    if (await launchUrl(Uri.parse(lang == "id"
+    final url = lang == "id"
         ? news?.detail?.where((e) => e.lang == "id").first.externalLink ?? ''
-        : news?.detail?.where((e) => e.lang == "en").first.externalLink ??
-            ''))) {
-      throw Exception(
-          'Could not launch ${lang == "id" ? news?.detail?.where((e) => e.lang == "id").first.externalLink : news?.detail?.where((e) => e.lang == "en").first.externalLink}');
+        : news?.detail?.where((e) => e.lang == "en").first.externalLink ?? '';
+
+    try {
+      await launchUrl(Uri.parse(url));
+    } catch (e) {
+      AppSnackBar.error('Failed to launch news link');
+      throw Exception('Could not launch $url');
     }
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {}
   }
 
   void _showImagePreview(BuildContext context, String imageUrl) {
