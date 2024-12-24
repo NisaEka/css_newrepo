3eefb0d9f759ea6a003c5cb69880bdb87891fff6
Ravenaa <ravena0402@gmail.com>
2024-12-24 11:23:48 +0700

    fix - update data lisensi - fix preview image on pantau detail - change popup image preview on document profile

--- lib/screen/profile/profil_menu/dokumen_screen.dart
+++ lib/screen/profile/profil_menu/dokumen_screen.dart
@@ -2,11 +2,14 @@ import 'package:css_mobile/const/app_const.dart';
 import 'package:css_mobile/const/color_const.dart';
 import 'package:css_mobile/screen/profile/profil_menu/dokumen_controller.dart';
 import 'package:css_mobile/widgets/bar/customtopbar.dart';
-import 'package:css_mobile/widgets/dialog/image_popup_dialog.dart';
 import 'package:css_mobile/widgets/dialog/secret_data_dialog.dart';
 import 'package:css_mobile/widgets/items/document_image_item.dart';
+import 'package:css_mobile/widgets/items/show_image_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:photo_view/photo_view.dart';
+// import 'package:photo_view/photo_view_gallery.dart';
 
 class DokumenScreen extends StatelessWidget {
   const DokumenScreen({super.key});
@@ -32,36 +35,30 @@ class DokumenScreen extends StatelessWidget {
             isLoading: c.isLoading,
             title: 'Lampiran Dokumen KTP'.tr,
             img: c.ccrfProfil?.document?.ccrfKtpattached ?? '',
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => ImagePopupDialog(
-                title: 'Lampiran Dokumen KTP'.tr,
-                img: c.ccrfProfil?.document?.ccrfKtpattached ?? '',
-              ),
+            onTap: () => showImagePreview(
+              context,
+              // 'Lampiran Dokumen KTP'.tr,
+              c.ccrfProfil?.document?.ccrfKtpattached ?? '',
             ),
           ),
           DocumentImageItem(
             isLoading: c.isLoading,
             title: 'Lampiran Dokumen NPWP'.tr,
             img: c.ccrfProfil?.document?.ccrfNpwpattached ?? '',
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => ImagePopupDialog(
-                title: 'Lampiran Dokumen NPWP'.tr,
-                img: c.ccrfProfil?.document?.ccrfNpwpattached ?? '',
-              ),
+            onTap: () => showImagePreview(
+              context,
+              // 'Lampiran Dokumen NPWP'.tr,
+              c.ccrfProfil?.document?.ccrfNpwpattached ?? '',
             ),
           ),
           DocumentImageItem(
             isLoading: c.isLoading,
             title: 'Lampiran Dokumen Rekening'.tr,
             img: c.ccrfProfil?.document?.ccrfAccountattached ?? '',
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => ImagePopupDialog(
-                title: 'Lampiran Dokumen Rekening'.tr,
-                img: c.ccrfProfil?.document?.ccrfAccountattached ?? '',
-              ),
+            onTap: () => showImagePreview(
+              context,
+              // 'Lampiran Dokumen Rekening'.tr,
+              c.ccrfProfil?.document?.ccrfAccountattached ?? '',
             ),
           ),
         ],
@@ -101,4 +98,44 @@ class DokumenScreen extends StatelessWidget {
       ],
     );
   }
+
+  // Function to show image preview
+  // void _showImagePreview(BuildContext context, String title, String imageUrl) {
+  //   if (imageUrl.isNotEmpty) {
+  //     Navigator.of(context).push(
+  //       MaterialPageRoute(
+  //         builder: (_) => Scaffold(
+  //           backgroundColor: Colors.black,
+  //           appBar: AppBar(
+  //             backgroundColor: Colors.black,
+  //             elevation: 0,
+  //             automaticallyImplyLeading: false,
+  //             actions: [
+  //               IconButton(
+  //                 icon: const Icon(Icons.close, color: whiteColor),
+  //                 onPressed: () => Navigator.of(context).pop(),
+  //               ),
+  //             ],
+  //           ),
+  //           body: Center(
+  //             child: PhotoViewGallery.builder(
+  //               itemCount: 1,
+  //               builder: (context, index) {
+  //                 return PhotoViewGalleryPageOptions(
+  //                   imageProvider: CachedNetworkImageProvider(imageUrl),
+  //                   minScale: PhotoViewComputedScale.contained,
+  //                   maxScale: PhotoViewComputedScale.covered * 3.0,
+  //                   initialScale: PhotoViewComputedScale.contained,
+  //                 );
+  //               },
+  //               scrollPhysics: const BouncingScrollPhysics(),
+  //               backgroundDecoration: const BoxDecoration(color: Colors.black),
+  //               pageController: PageController(),
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //     );
+  //   }
+  // }
 }
