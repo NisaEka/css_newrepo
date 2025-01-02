db0fe2f093e79bda98e8046c962a2c3af07e800f
Ravenaa <ravena0402@gmail.com>
2025-01-02 17:13:14 +0700

    fix on screen notification_controller

--- lib/screen/auth/login/login_controller.dart
+++ lib/screen/auth/login/login_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:css_mobile/base/base_controller.dart';
-import 'package:css_mobile/const/color_const.dart';
 import 'package:css_mobile/data/model/auth/get_device_info_model.dart';
 import 'package:css_mobile/data/model/auth/input_login_model.dart';
 import 'package:css_mobile/data/model/auth/input_pinconfirm_model.dart';
@@ -41,35 +40,35 @@ class LoginController extends BaseController {
     ValidationBuilder.setLocale(state.lang!);
   }
 
-  bool onPop() {
-    DateTime now = DateTime.now();
-    if (state.currentBackPressTime == null ||
-        now.difference(state.currentBackPressTime!) >
-            const Duration(seconds: 2)) {
-      state.currentBackPressTime = now;
-      AppSnackBar.custom(
-        message: 'Double click back button to exit'.tr,
-        backgroundColor: greyColor.withOpacity(0.8),
-        icon: const Icon(
-          Icons.info,
-          color: whiteColor,
-        ),
-        durationInSeconds: 3,
-        snackPosition: SnackPosition.BOTTOM,
-        snackStyle: SnackStyle.GROUNDED,
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
-        padding: const EdgeInsets.all(10),
-        messageText: null,
-      );
-      state.pop = false;
-      update();
-      return false;
-    }
-    state.pop = true;
-    update();
-    Get.off(() => const DashboardScreen(), arguments: {'isFirstLogin': true});
-    return true;
-  }
+  // bool onPop() {
+  //   DateTime now = DateTime.now();
+  //   if (state.currentBackPressTime == null ||
+  //       now.difference(state.currentBackPressTime!) >
+  //           const Duration(seconds: 2)) {
+  //     state.currentBackPressTime = now;
+  //     AppSnackBar.custom(
+  //       message: 'Double click back button to exit'.tr,
+  //       backgroundColor: greyColor.withOpacity(0.8),
+  //       icon: const Icon(
+  //         Icons.info,
+  //         color: whiteColor,
+  //       ),
+  //       durationInSeconds: 3,
+  //       snackPosition: SnackPosition.BOTTOM,
+  //       snackStyle: SnackStyle.GROUNDED,
+  //       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+  //       padding: const EdgeInsets.all(10),
+  //       messageText: null,
+  //     );
+  //     state.pop = false;
+  //     update();
+  //     return false;
+  //   }
+  //   state.pop = true;
+  //   update();
+  //   Get.off(() => const DashboardScreen(), arguments: {'isFirstLogin': true});
+  //   return true;
+  // }
 
   Widget showIcon = const Icon(
     Icons.remove_red_eye,
@@ -179,22 +178,22 @@ class LoginController extends BaseController {
     return null;
   }
 
-  Future cekToken() async {
-    state.isLoading = true;
-    update();
-    String? token = await storage.readAccessToken();
-    AppLogger.e('token : $token');
-    if (token != null) {
-      Get.delete<DashboardController>().then((_) => Get.offAll(
-          () => const DashboardScreen(),
-          arguments: {'isFirstLogin': true}));
-      // String all = await storage.readString(StorageCore.allowedMenu);
-      // AllowedMenu menu = AllowedMenu.fromJson(jsonDecode(all));
-      // print(menu.beranda);
-    }
-    state.isLoading = false;
-    update();
-  }
+  // Future cekToken() async {
+  //   state.isLoading = true;
+  //   update();
+  //   String? token = await storage.readAccessToken();
+  //   AppLogger.e('token : $token');
+  //   if (token != null) {
+  //     Get.delete<DashboardController>().then((_) => Get.offAll(
+  //         () => const DashboardScreen(),
+  //         arguments: {'isFirstLogin': true}));
+  //     // String all = await storage.readString(StorageCore.allowedMenu);
+  //     // AllowedMenu menu = AllowedMenu.fromJson(jsonDecode(all));
+  //     // print(menu.beranda);
+  //   }
+  //   state.isLoading = false;
+  //   update();
+  // }
 
   Future<Coordinate> getCurrentLocation() async {
     bool serviceEnabled;
