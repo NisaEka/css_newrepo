4668cc66c1d2be9360b33746e10ae3c70264b44f
nisae <nisa.eka.juliana@gmail.com>
2025-01-07 15:05:30 +0700

    fix: - overflow on dokumen screen

--- lib/screen/profile/profil_menu/dokumen_screen.dart
+++ lib/screen/profile/profil_menu/dokumen_screen.dart
@@ -7,6 +7,7 @@ import 'package:css_mobile/widgets/items/document_image_item.dart';
 import 'package:css_mobile/widgets/items/show_image_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 // import 'package:cached_network_image/cached_network_image.dart';
 // import 'package:photo_view/photo_view.dart';
 // import 'package:photo_view/photo_view_gallery.dart';
@@ -101,43 +102,43 @@ class DokumenScreen extends StatelessWidget {
     );
   }
 
-  // Function to show image preview
-  // void _showImagePreview(BuildContext context, String title, String imageUrl) {
-  //   if (imageUrl.isNotEmpty) {
-  //     Navigator.of(context).push(
-  //       MaterialPageRoute(
-  //         builder: (_) => Scaffold(
-  //           backgroundColor: Colors.black,
-  //           appBar: AppBar(
-  //             backgroundColor: Colors.black,
-  //             elevation: 0,
-  //             automaticallyImplyLeading: false,
-  //             actions: [
-  //               IconButton(
-  //                 icon: const Icon(Icons.close, color: whiteColor),
-  //                 onPressed: () => Navigator.of(context).pop(),
-  //               ),
-  //             ],
-  //           ),
-  //           body: Center(
-  //             child: PhotoViewGallery.builder(
-  //               itemCount: 1,
-  //               builder: (context, index) {
-  //                 return PhotoViewGalleryPageOptions(
-  //                   imageProvider: CachedNetworkImageProvider(imageUrl),
-  //                   minScale: PhotoViewComputedScale.contained,
-  //                   maxScale: PhotoViewComputedScale.covered * 3.0,
-  //                   initialScale: PhotoViewComputedScale.contained,
-  //                 );
-  //               },
-  //               scrollPhysics: const BouncingScrollPhysics(),
-  //               backgroundDecoration: const BoxDecoration(color: Colors.black),
-  //               pageController: PageController(),
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //     );
-  //   }
-  // }
+// Function to show image preview
+// void _showImagePreview(BuildContext context, String title, String imageUrl) {
+//   if (imageUrl.isNotEmpty) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (_) => Scaffold(
+//           backgroundColor: Colors.black,
+//           appBar: AppBar(
+//             backgroundColor: Colors.black,
+//             elevation: 0,
+//             automaticallyImplyLeading: false,
+//             actions: [
+//               IconButton(
+//                 icon: const Icon(Icons.close, color: whiteColor),
+//                 onPressed: () => Navigator.of(context).pop(),
+//               ),
+//             ],
+//           ),
+//           body: Center(
+//             child: PhotoViewGallery.builder(
+//               itemCount: 1,
+//               builder: (context, index) {
+//                 return PhotoViewGalleryPageOptions(
+//                   imageProvider: CachedNetworkImageProvider(imageUrl),
+//                   minScale: PhotoViewComputedScale.contained,
+//                   maxScale: PhotoViewComputedScale.covered * 3.0,
+//                   initialScale: PhotoViewComputedScale.contained,
+//                 );
+//               },
+//               scrollPhysics: const BouncingScrollPhysics(),
+//               backgroundDecoration: const BoxDecoration(color: Colors.black),
+//               pageController: PageController(),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 }
